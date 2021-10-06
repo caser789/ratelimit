3df2dc931bffac6d06911d974944ed57fad89375
Xue Jiao <jiao.xue@seamoney.com>
2021-10-06 18:24:34 +0800

    final

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module caser789/ratelimit
 
 go 1.16
 
-require launchpad.net/gocheck v0.0.0-20140225173054-000000000087
+require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c
--- ratelimit_test.go
+++ ratelimit_test.go
@@ -5,10 +5,11 @@
 package ratelimit
 
 import (
-	gc "launchpad.net/gocheck"
-
+	"math"
 	"testing"
 	"time"
+
+	gc "gopkg.in/check.v1"
 )
 
 func TestPackage(t *testing.T) {
@@ -123,6 +124,49 @@ var takeTests = []struct {
 		count:      2,
 		expectWait: 10 * time.Millisecond,
 	}},
+}}
+
+var availTests = []struct {
+	about        string
+	capacity     int64
+	fillInterval time.Duration
+	take         int64
+	sleep        time.Duration
+
+	expectCountAfterTake  int64
+	expectCountAfterSleep int64
+}{{
+	about:                 "should fill tokens after interval",
+	capacity:              5,
+	fillInterval:          time.Second,
+	take:                  5,
+	sleep:                 time.Second,
+	expectCountAfterTake:  0,
+	expectCountAfterSleep: 1,
+}, {
+	about:                 "should fill tokens plus existing count",
+	capacity:              2,
+	fillInterval:          time.Second,
+	take:                  1,
+	sleep:                 time.Second,
+	expectCountAfterTake:  1,
+	expectCountAfterSleep: 2,
+}, {
+	about:                 "shouldn't fill before interval",
+	capacity:              2,
+	fillInterval:          2 * time.Second,
+	take:                  1,
+	sleep:                 time.Second,
+	expectCountAfterTake:  1,
+	expectCountAfterSleep: 1,
+}, {
+	about:                 "should fill only once after 1*interval before 2*interval",
+	capacity:              2,
+	fillInterval:          2 * time.Second,
+	take:                  1,
+	sleep:                 3 * time.Second,
+	expectCountAfterTake:  1,
+	expectCountAfterSleep: 2,
 }}
 
 func (rateLimitSuite) TestTake(c *gc.C) {
@@ -261,7 +305,7 @@ func (rateLimitSuite) TestPanics(c *gc.C) {
 }
 
 func isCloseTo(x, y, tolerance float64) bool {
-	return abs(x-y)/y < tolerance
+	return math.Abs(x-y)/y < tolerance
 }
 
 func (rateLimitSuite) TestRate(c *gc.C) {
@@ -273,7 +317,7 @@ func (rateLimitSuite) TestRate(c *gc.C) {
 	if !isCloseTo(tb.Rate(), 0.5, 0.00001) {
 		c.Fatalf("got %v want 0.5", tb.Rate())
 	}
-	tb = newBucketWithQuantum(100*time.Millisecond, 1, 5)
+	tb = NewBucketWithQuantum(100*time.Millisecond, 1, 5)
 	if !isCloseTo(tb.Rate(), 50, 0.00001) {
 		c.Fatalf("got %v want 50", tb.Rate())
 	}
@@ -300,7 +344,7 @@ func checkRate(c *gc.C, rate float64) {
 	}
 }
 
-func (rateLimitSuite) TestNewWithRate(c *gc.C) {
+func (rateLimitSuite) NewBucketWithRate(c *gc.C) {
 	for rate := float64(1); rate < 1e6; rate += 7 {
 		checkRate(c, rate)
 	}
@@ -313,6 +357,7 @@ func (rateLimitSuite) TestNewWithRate(c *gc.C) {
 		0.9e8,
 		3e12,
 		4e18,
+		float64(1<<63 - 1),
 	} {
 		checkRate(c, rate)
 		checkRate(c, rate/3)
@@ -320,9 +365,57 @@ func (rateLimitSuite) TestNewWithRate(c *gc.C) {
 	}
 }
 
+func TestAvailable(t *testing.T) {
+	for i, tt := range availTests {
+		tb := NewBucket(tt.fillInterval, tt.capacity)
+		if c := tb.takeAvailable(tb.startTime, tt.take); c != tt.take {
+			t.Fatalf("#%d: %s, take = %d, want = %d", i, tt.about, c, tt.take)
+		}
+		if c := tb.available(tb.startTime); c != tt.expectCountAfterTake {
+			t.Fatalf("#%d: %s, after take, available = %d, want = %d", i, tt.about, c, tt.expectCountAfterTake)
+		}
+		if c := tb.available(tb.startTime.Add(tt.sleep)); c != tt.expectCountAfterSleep {
+			t.Fatalf("#%d: %s, after some time it should fill in new tokens, available = %d, want = %d",
+				i, tt.about, c, tt.expectCountAfterSleep)
+		}
+	}
+
+}
+
+func TestNoBonusTokenAfterBucketIsFull(t *testing.T) {
+	tb := NewBucketWithQuantum(time.Second*1, 100, 20)
+	curAvail := tb.Available()
+	if curAvail != 100 {
+		t.Fatalf("initially: actual available = %d, expected = %d", curAvail, 100)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	curAvail = tb.Available()
+	if curAvail != 100 {
+		t.Fatalf("after pause: actual available = %d, expected = %d", curAvail, 100)
+	}
+
+	cnt := tb.TakeAvailable(100)
+	if cnt != 100 {
+		t.Fatalf("taking: actual taken count = %d, expected = %d", cnt, 100)
+	}
+
+	curAvail = tb.Available()
+	if curAvail != 0 {
+		t.Fatalf("after taken: actual available = %d, expected = %d", curAvail, 0)
+	}
+}
+
 func BenchmarkWait(b *testing.B) {
 	tb := NewBucket(1, 16*1024)
 	for i := b.N - 1; i >= 0; i-- {
 		tb.Wait(1)
+	}
+}
+
+func BenchmarkNewBucket(b *testing.B) {
+	for i := b.N - 1; i >= 0; i-- {
+		NewBucketWithRate(4e18, 1<<62)
 	}
 }
